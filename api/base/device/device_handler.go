9305ebac4ae287fc2b350bdc68538aef46a0fd8c
Mitchell-Tol <mitchelltol02@gmail.com>
2024-05-25 16:00:12 +0200

    update devices

--- api/base/device/device_handler.go
+++ api/base/device/device_handler.go
@@ -65,3 +65,30 @@ func (h *DeviceHandler) CreateDevice(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
     w.Write(jsonResp)
 }
+
+func (h *DeviceHandler) UpdateDevice(w http.ResponseWriter, r *http.Request) {
+	body := &devicemodel.UpdatedDevice{}
+	reqErr := json.NewDecoder(r.Body).Decode(body)
+	if reqErr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(generic.JSONError("Invalid JSON"))
+		return
+	}
+
+	result, sqlErr := h.store.UpdateDevice(body)
+	if sqlErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(generic.JSONError("An error occurred while updating the device"))
+		return
+	}
+
+	jsonResponse, jsonErr := json.Marshal(result)
+	if jsonErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(generic.JSONError("An error occurred while mapping the response to JSON"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+}
